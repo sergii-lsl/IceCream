58dacd825d1527107e0aa8adf16d2696ef1648ec
Musiyan98 <musiyan98@gmail.com>
2022-06-28 03:56:49 +0300

    update modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,12 @@
 (() => {
     const refs = {
+      openModalBtnMob: document.querySelector("[data-modal-mob-open]"),
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
   
+    refs.openModalBtnMob.addEventListener("click", toggleModal);
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
