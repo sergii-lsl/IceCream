a81b1639b2c3fcfb1fe900b38a2f783486194b2f
Musiyan98 <musiyan98@gmail.com>
2022-06-28 15:43:16 +0300

    update script

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,16 @@
 (() => {
     const refs = {
       openModalBtnMob: document.querySelector("[data-modal-mob-open]"),
-      openModalBtn: document.querySelector("[data-modal-open]"),
+      openModalBtnTab: document.querySelector("[data-modal-open-tablet]"),
+      openModalBtnDesk: document.querySelector("[data-modal-open-desktop]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       openModalSuccessBtn: document.querySelector("[data-modal-success-open]"),
       modal: document.querySelector("[data-modal]"),
     };
   
     refs.openModalBtnMob.addEventListener("click", toggleModal);
-    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtnTab.addEventListener("click", toggleModal);
+    refs.openModalBtnDesk.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
     refs.openModalSuccessBtn.addEventListener("click", toggleModal);
   
